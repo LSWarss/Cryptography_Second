58a80628a1b481a5bf392577226b31aac77f00bb
LSWarss <ls.warss98@gmail.com>
2021-03-11 19:43:42 +0100

    Adding rest of the assignment

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
-from utility import hashWithAllMethods
+from utility import hashWithAllMethods, measure_time_of_hasing_with_single_hashing
 import time
 
 start, times = time.perf_counter(), {}
 
 testString = b"Obi-Wan Kenobi: It's over Anakin, I have the high ground. ... Obi-Wan Kenobi: YOU WERE THE CHOSEN ONE! It was said that you would destroy the Sith, not join them, bring balance to the force, not leave it in darkness. Anakin Skywalker/Darth Vader: I HATE YOU!"
 
-hashWithAllMethods(testString)
+# hashWithAllMethods(testString)
+# print(measure_time_of_hashing(testString))
+print(measure_time_of_hasing_with_single_hashing(testString))
--- src/utility.py
+++ src/utility.py
@@ -1,4 +1,6 @@
 import hashlib
+import timeit
+import plotly.express as px
 
 blake2b = hashlib.blake2b()
 blake2s = hashlib.blake2s()
@@ -20,6 +22,31 @@ hash_list = [blake2b,blake2s,md5,sha1,sha224,sha256,sha384,sha3224,sha3256,sha33
 def hashWithAllMethods(text):
     for hash_object in hash_list:
         hash_object.update(text)
-        print('{}: {}'.format(hash_object.name, hash_object.hexdigest())) 
-    
-    
+        if(hash_object == shake128 or hash_object == shake256):
+            print('{}: {}'.format(hash_object.name, hash_object.hexdigest(20))) 
+        else:
+            print('{}: {}'.format(hash_object.name, hash_object.hexdigest())) 
+
+def measure_time_of_hashing_with_all(text):
+    return timeit.timeit(lambda: hashWithAllMethods(text), number=1000)
+
+def measure_time_of_hasing_with_single_hashing(text): 
+    measureTimes = {}
+    for hash_object in hash_list:
+        measureTimes[hash_object.name] = timeit.timeit(lambda: hash_object.update(text))
+    return measureTimes
+
+def fileHashing(path: str, hashingName: str) -> str:
+    for hashFunction in hash_list:
+        if hashFunction.name == hashingName:
+            with open(path, 'rb') as file: 
+                while True:
+                    chunk = file.read(hashFunction.block_size)
+                    if not chunk:
+                        break
+                    hashFunction.update(chunk)
+    return hashFunction.hexdigest(20) if "shake_" in hashFunction.name else hashFunction.hexdigest()
+
+def plot_times_of_hashes(text):
+    fig = px.bar(measure_time_of_hasing_with_single_hashing(text), x='times', y='y_vals')
+    fig.show()
